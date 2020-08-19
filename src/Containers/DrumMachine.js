2af908694552a13499d5a1736ec6027f7efb5356
Maciek Sitkowski <msitkowski94@gmail.com>
2020-08-19 17:01:25 +0200

    Add loadedSounds piece of state

--- src/Containers/DrumMachine.js
+++ src/Containers/DrumMachine.js
@@ -8,35 +8,39 @@ import PowerSwitch from '../components/PowerSwitch';
 import SoundsSwitch from '../components/SoundsSwitch';
 import PadBank from '../components/PadBank';
 
+import { drumKitSounds, oldschoolSounds } from '../sounds';
+
 export default function DrumMachine() {
   // If hasPower is true then app is active
-  const [hasPower, setHasPower] = useState(true);
+  const [hasPower, setHasPower] = useState(false);
   const togglePower = () => {
     setHasPower(!hasPower);
+    setDisplayText(hasPower ? 'Power OFF' : 'Power ON');
+  };
+
+  // Display
+  const [displayText, setDisplayText] = useState('');
+
+  // Loaded sounds
+  const [loadedSounds, setLoadedSounds] = useState(drumKitSounds);
+  const toggleSounds = () => {
+    if (loadedSounds.id === drumKitSounds.id) {
+      setLoadedSounds(oldschoolSounds);
+      setDisplayText(oldschoolSounds.label);
+    } else {
+      setLoadedSounds(drumKitSounds);
+      setDisplayText(drumKitSounds.label);
+    }
   };
 
   return (
     <Paper className="DrumMachine" elevation={5} id="drum-machine">
-      <PadBank 
-        isDrumPadDisabled={!hasPower}
-      />
+      <PadBank sounds={loadedSounds.sounds} isDrumPadDisabled={!hasPower} />
       <Title>Drum Machine</Title>
-      <Display 
-        disabled={!hasPower} text="Hip Hop" 
-      />
-      <VolumeSlider 
-        value={50} 
-        onChange={() => 'TODO'} 
-        disabled={!hasPower}
-      />
-      <PowerSwitch 
-        checked={hasPower} 
-        onChange={togglePower}
-      />
-      <SoundsSwitch 
-        disabled={!hasPower}
-        onChange={() => 'TODO'}  
-      />
+      <Display disabled={!hasPower} text={displayText} />
+      <VolumeSlider value={50} onChange={() => 'TODO'} disabled={!hasPower} />
+      <PowerSwitch checked={hasPower} onChange={togglePower} />
+      <SoundsSwitch disabled={!hasPower} onChange={toggleSounds} />
     </Paper>
   );
 }
--- src/sounds.js
+++ src/sounds.js
@@ -1,86 +1,94 @@
-export const defaultSounds = [
-  {
-    label: 'Bap',
-    src: 'http://sitek94.vot.pl/assets/drum-machine/power-drumkit/bap.wav',
-  },
-  {
-    label: 'Bongo',
-    src: 'http://sitek94.vot.pl/assets/drum-machine/power-drumkit/bongo.wav',
-  },
-  {
-    label: 'China',
-    src: 'http://sitek94.vot.pl/assets/drum-machine/power-drumkit/china.wav',
-  },
-  {
-    label: 'Clap',
-    src: 'http://sitek94.vot.pl/assets/drum-machine/power-drumkit/clap.wav',
-  },
-  {
-    label: 'Crash',
-    src: 'http://sitek94.vot.pl/assets/drum-machine/power-drumkit/crash.wav',
-  },
-  {
-    label: 'Tom-Hi',
-    src: 'http://sitek94.vot.pl/assets/drum-machine/power-drumkit/tom-hi.wav',
-  },
-  {
-    label: 'Tom-Hi-Two',
-    src: 'http://sitek94.vot.pl/assets/drum-machine/power-drumkit/tom-hi2.wav',
-  },
-  {
-    label: 'Tom-Low',
-    src: 'http://sitek94.vot.pl/assets/drum-machine/power-drumkit/tom-low.wav',
-  },
-  {
-    label: 'Tom-Low-Two',
-    src: 'http://sitek94.vot.pl/assets/drum-machine/power-drumkit/tom-low2.wav',
-  },
-];
+export const drumKitSounds = {
+  id: 0,
+  label: 'Power Drum Kit',
+  sounds: [
+    {
+      label: 'Bap',
+      src: 'http://sitek94.vot.pl/assets/drum-machine/power-drumkit/bap.wav',
+    },
+    {
+      label: 'Bongo',
+      src: 'http://sitek94.vot.pl/assets/drum-machine/power-drumkit/bongo.wav',
+    },
+    {
+      label: 'China',
+      src: 'http://sitek94.vot.pl/assets/drum-machine/power-drumkit/china.wav',
+    },
+    {
+      label: 'Clap',
+      src: 'http://sitek94.vot.pl/assets/drum-machine/power-drumkit/clap.wav',
+    },
+    {
+      label: 'Crash',
+      src: 'http://sitek94.vot.pl/assets/drum-machine/power-drumkit/crash.wav',
+    },
+    {
+      label: 'Tom-Hi',
+      src: 'http://sitek94.vot.pl/assets/drum-machine/power-drumkit/tom-hi.wav',
+    },
+    {
+      label: 'Tom-Hi-Two',
+      src: 'http://sitek94.vot.pl/assets/drum-machine/power-drumkit/tom-hi2.wav',
+    },
+    {
+      label: 'Tom-Low',
+      src: 'http://sitek94.vot.pl/assets/drum-machine/power-drumkit/tom-low.wav',
+    },
+    {
+      label: 'Tom-Low-Two',
+      src: 'http://sitek94.vot.pl/assets/drum-machine/power-drumkit/tom-low2.wav',
+    },
+  ]
+}
 
-export const oldschoolSounds = [
-  {
-    label: 'Clock',
-    src:
-      'http://sitek94.vot.pl/assets/drum-machine/hip-hop-speciality-kit/clock.wav',
-  },
-  {
-    label: 'Drop-Kick',
-    src:
-      'http://sitek94.vot.pl/assets/drum-machine/hip-hop-speciality-kit/drop-kick.wav',
-  },
-  {
-    label: 'Kick-Punch',
-    src:
-      'http://sitek94.vot.pl/assets/drum-machine/hip-hop-speciality-kit/kick-punch.wav',
-  },
-  {
-    label: 'Say-Bow',
-    src:
-      'http://sitek94.vot.pl/assets/drum-machine/hip-hop-speciality-kit/say-bow-beep.wav',
-  },
-  {
-    label: 'Snappin-Bap',
-    src:
-      'http://sitek94.vot.pl/assets/drum-machine/hip-hop-speciality-kit/snappin-bap.wav',
-  },
-  {
-    label: 'Tabla-Hit',
-    src:
-      'http://sitek94.vot.pl/assets/drum-machine/hip-hop-speciality-kit/tabla-hit.wav',
-  },
-  {
-    label: 'Thud-Kick',
-    src:
-      'http://sitek94.vot.pl/assets/drum-machine/hip-hop-speciality-kit/thud-kick.wav',
-  },
-  {
-    label: 'Thud-Snare',
-    src:
-      'http://sitek94.vot.pl/assets/drum-machine/hip-hop-speciality-kit/thud-snare.wav',
-  },
-  {
-    label: 'Trigger-Bap',
-    src:
-      'http://sitek94.vot.pl/assets/drum-machine/hip-hop-speciality-kit/trigger-bap.wav',
-  },
-];
+export const oldschoolSounds = {
+  id: 1,
+  label: 'Old School Vibes',
+  sounds: [
+    {
+      label: 'Clock',
+      src:
+        'http://sitek94.vot.pl/assets/drum-machine/hip-hop-speciality-kit/clock.wav',
+    },
+    {
+      label: 'Drop-Kick',
+      src:
+        'http://sitek94.vot.pl/assets/drum-machine/hip-hop-speciality-kit/drop-kick.wav',
+    },
+    {
+      label: 'Kick-Punch',
+      src:
+        'http://sitek94.vot.pl/assets/drum-machine/hip-hop-speciality-kit/kick-punch.wav',
+    },
+    {
+      label: 'Say-Bow',
+      src:
+        'http://sitek94.vot.pl/assets/drum-machine/hip-hop-speciality-kit/say-bow-beep.wav',
+    },
+    {
+      label: 'Snappin-Bap',
+      src:
+        'http://sitek94.vot.pl/assets/drum-machine/hip-hop-speciality-kit/snappin-bap.wav',
+    },
+    {
+      label: 'Tabla-Hit',
+      src:
+        'http://sitek94.vot.pl/assets/drum-machine/hip-hop-speciality-kit/tabla-hit.wav',
+    },
+    {
+      label: 'Thud-Kick',
+      src:
+        'http://sitek94.vot.pl/assets/drum-machine/hip-hop-speciality-kit/thud-kick.wav',
+    },
+    {
+      label: 'Thud-Snare',
+      src:
+        'http://sitek94.vot.pl/assets/drum-machine/hip-hop-speciality-kit/thud-snare.wav',
+    },
+    {
+      label: 'Trigger-Bap',
+      src:
+        'http://sitek94.vot.pl/assets/drum-machine/hip-hop-speciality-kit/trigger-bap.wav',
+    },
+  ]
+};
