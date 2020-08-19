4947904b5252d1cc4767e799a074e1e00b9e4d19
Maciek Sitkowski <msitkowski94@gmail.com>
2020-08-19 13:27:45 +0200

    Create basic layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,125 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, createMuiTheme, CssBaseline } from '@material-ui/core';
 
-function App() {
+import {
+  Paper,
+  Typography,
+  Slider,
+  FormControlLabel,
+  Switch,
+} from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#61dafb'
+    }
+  },
+});
+
+function BankSwitch({ power, onSoundsToggle }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <FormControlLabel
+      className="BankSwitch"
+      control={<Switch disabled={!power} onChange={onSoundsToggle} />}
+      label="Bank"
+      labelPlacement="top"
+    />
+  );
+}
+
+function PowerSwitch({ power, onPowerToggle }) {
+  return (
+    <FormControlLabel
+      className="PowerSwitch"
+      control={<Switch checked={power} onChange={onPowerToggle} />}
+      label="Power"
+      labelPlacement="top"
+    />
+  );
+}
+
+function VolumeSlider({ value, onChange, disabled }) {
+  return (
+    <div className="VolumeSlider">
+      <Typography>Volume</Typography>
+      <Slider
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+        valueLabelDisplay="auto"
+        valueLabelFormat={(x) => Math.round(x * 100)}
+        min={0}
+        max={1}
+        step={0.01}
+      />
     </div>
   );
 }
 
-export default App;
+function Display({ power, text }) {
+  return (
+    <Paper
+      className="Display"
+      variant={power ? 'elevation' : 'outlined'}
+      elevation={power ? 9 : 3}
+    >
+      <Typography
+        color={power ? 'initial' : 'textSecondary'}
+        id="display"
+        variant="h6"
+      >
+        {text}
+      </Typography>
+    </Paper>
+  );
+}
+
+function Controls() {
+  return (
+    <div className="Controls">
+      <h1 className="Title">Drum Machine</h1>
+      <Display power text="Hip Hop" />
+      <VolumeSlider value={50} onChange={() => "TODO"} disabled={false} />
+      <PowerSwitch />
+      <BankSwitch />
+    </div>
+  );
+}
+
+function DrumPad({ children }) {
+  return <div className="DrumPad">{children}</div>;
+}
+
+function PadBank() {
+  const keys = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
+
+  return (
+    <div className="PadBank">
+      {keys.map((key) => (
+        <DrumPad key={key}>{key}</DrumPad>
+      ))}
+    </div>
+  );
+}
+
+function DrumMachine() {
+  return (
+    <Paper className="DrumMachine" elevation={5} id="drum-machine">
+      <PadBank />
+      <Controls />
+    </Paper>
+  );
+}
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <CssBaseline />
+        <DrumMachine />
+      </div>
+    </ThemeProvider>
+  );
+}
